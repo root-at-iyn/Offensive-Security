104fae36de91e922db30a2de148ddfbea379591f
root-at-iyn <insideyournetwork@gmail.com>
2025-12-10 01:38:46 +0000

    challenge 9

---
+++ Cryptography/Cryptopals/Set_2/challenge_9.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# Implement PKCS#7 padding
+
+
+def pkcs7(block: bytes, blocksize: int) -> bytes:
+    if len(block) % blocksize != 0:
+        padsize = blocksize - (len(block) % blocksize)
+        padding = b"\x07" * padsize
+        return block + padding 
+    else:
+        return block
+
+if __name__ == "__main__":
+    ys = b"YELLOW SUBMARINE AND 12345"
+    blksize = 16
+    print(pkcs7(ys, 16))
+    print(f"Bytes Length: {len(ys)}")
+    print(f"Pad Length: {blksize - (len(ys) % blksize)}")
+    print(f"Block Size: {blksize}")
+
+
+# Expected Output:
+
+#root-at-iyn@Ubuntu-220403-x86-64:~/Cryptography/Cryptopals/Set2$ ./challenge_9.py
+#b'YELLOW SUBMARINE AND 12345\x07\x07\x07\x07\x07\x07'
+#Bytes Length: 26
+#Pad Length: 6
+#Block Size: 16
+
