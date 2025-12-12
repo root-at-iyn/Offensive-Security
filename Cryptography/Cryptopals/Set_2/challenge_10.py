13e02063a42934d8b177295d235ad880212bc939
root-at-iyn <insideyournetwork@gmail.com>
2025-12-12 15:34:07 +0000

    fix challenge_10.py

--- Cryptography/Cryptopals/Set_2/challenge_10.py
+++ Cryptography/Cryptopals/Set_2/challenge_10.py
@@ -11,6 +11,7 @@
 
 import base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from challenge_9 import pkcs7_pad, pkcs7_unpad
 
 def get_filedata(filename: str) -> bytes:
     with open(filename, "rb") as fp:
@@ -36,12 +37,12 @@ def decrypt_cbc_data(ciphertext: bytes, key: bytes, iv: bytes) -> bytes:
         pt_blk_i_int = int.from_bytes(decryptor.update(ciphertext[ct:ct+blocksize]))
         ct_blk_i_int = int.from_bytes(ciphertext[(ct-blocksize):(ct-blocksize)+blocksize])
         plaintext += (pt_blk_i_int ^ ct_blk_i_int).to_bytes(length=blocksize) 
-    return  plaintext + decryptor.finalize()
+    return  pkcs7_unpad(plaintext + decryptor.finalize()).decode('ascii')
     
 
 def encrypt_cbc_data(plaintext: bytes, key: bytes, iv: bytes) -> bytes:
     blocksize = 16
-    pt = pkcs7(plaintext, blocksize) # padded plaintext
+    pt = pkcs7_pad(plaintext) # padded plaintext
     cipher = Cipher(algorithms.AES(key), modes.ECB())
     encryptor = cipher.encryptor()
     pt_blk_0_int = int.from_bytes(pt[0:blocksize])
@@ -60,15 +61,94 @@ if __name__ == "__main__":
     PT1 = get_filedata("./test.txt")
     ENC_DATA = get_filedata("./10.txt")
     CT1 = encrypt_cbc_data(PT1, KEY, IV)
-    print(f"Decrypted Plaintext 1: {decrypt_cbc_data(CT1, KEY, IV)}\n")
-    print(f"Decrypted Plaintext 2:\n {decrypt_cbc_data(ENC_DATA, KEY, IV)}")
+    print(f"Decrypted Plaintext 1:\n{decrypt_cbc_data(CT1, KEY, IV)}")
+    print(f"Decrypted Plaintext 2:\n{decrypt_cbc_data(ENC_DATA, KEY, IV)}")
 
 
 # Expected Output:
 
-#root-at-iyn@Ubuntu-220403-x86-64:~/Cryptography/Cryptopals/Set2$ ./challenge_10.py
-#Decrypted Plaintext 1: b'CRYPTOPALS CHALLENGE 10\n\x08\x08\x08\x08\x08\x08\x08\x08'
+#root-at-iyn@Ubuntu-220403-x86-64:~/Cryptography/Cryptopals/Set2$ ./challenge_10.py 
+#Decrypted Plaintext 1:
+#CRYPTOPALS CHALLENGE 10
 #
 #Decrypted Plaintext 2:
-# b"I'm back and I'm ringin' the bell \nA rockin' on the mike while the fly girls yell \nIn ecstasy in the back of me \nWell that's my DJ Deshay cuttin' all them Z's \nHittin' hard and the girlies goin' crazy \nVanilla's on the mike, man I'm not lazy. \n\nI'm lettin' my drug kick in \nIt controls my mouth and I begin \nTo just let it flow, let my concepts go \nMy posse's to the side yellin', Go Vanilla Go! \n\nSmooth 'cause that's the way I will be \nAnd if you don't give a damn, then \nWhy you starin' at me \nSo get off 'cause I control the stage \nThere's no dissin' allowed \nI'm in my own phase \nThe girlies sa y they love me and that is ok \nAnd I can dance better than any kid n' play \n\nStage 2 -- Yea the one ya' wanna listen to \nIt's off my head so let the beat play through \nSo I can funk it up and make it sound good \n1-2-3 Yo -- Knock on some wood \nFor good luck, I like my rhymes atrocious \nSupercalafragilisticexpialidocious \nI'm an effect and that you can bet \nI can take a fly girl and make her wet. \n\nI'm like Samson -- Samson to Delilah \nThere's no denyin', You can try to hang \nBut you'll keep tryin' to get my style \nOver and over, practice makes perfect \nBut not if you're a loafer. \n\nYou'll get nowhere, no place, no time, no girls \nSoon -- Oh my God, homebody, you probably eat \nSpaghetti with a spoon! Come on and say it! \n\nVIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino \nIntoxicating so you stagger like a wino \nSo punks stop trying and girl stop cryin' \nVanilla Ice is sellin' and you people are buyin' \n'Cause why the freaks are jockin' like Crazy Glue \nMovin' and groovin' trying to sing along \nAll through the ghetto groovin' this here song \nNow you're amazed by the VIP posse. \n\nSteppin' so hard like a German Nazi \nStartled by the bases hittin' ground \nThere's no trippin' on mine, I'm just gettin' down \nSparkamatic, I'm hangin' tight like a fanatic \nYou trapped me once and I thought that \nYou might have it \nSo step down and lend me your ear \n'89 in my time! You, '90 is my year. \n\nYou're weakenin' fast, YO! and I can tell it \nYour body's gettin' hot, so, so I can smell it \nSo don't be mad and don't be sad \n'Cause the lyrics belong to ICE, You can call me Dad \nYou're pitchin' a fit, so step back and endure \nLet the witch doctor, Ice, do the dance to cure \nSo come up close and don't be square \nYou wanna battle me -- Anytime, anywhere \n\nYou thought that I was weak, Boy, you're dead wrong \nSo come on, everybody and sing this song \n\nSay -- Play that funky music Say, go white boy, go white boy go \nplay that funky music Go white boy, go white boy, go \nLay down and boogie and play that funky music till you die. \n\nPlay that funky music Come on, Come on, let me hear \nPlay that funky music white boy you say it, say it \nPlay that funky music A little louder now \nPlay that funky music, white boy Come on, Come on, Come on \nPlay that funky music \n\x04\x04\x04\x04"
+#I'm back and I'm ringin' the bell 
+#A rockin' on the mike while the fly girls yell 
+#In ecstasy in the back of me 
+#Well that's my DJ Deshay cuttin' all them Z's 
+#Hittin' hard and the girlies goin' crazy 
+#Vanilla's on the mike, man I'm not lazy. 
+#
+#I'm lettin' my drug kick in 
+#It controls my mouth and I begin 
+#To just let it flow, let my concepts go 
+#My posse's to the side yellin', Go Vanilla Go! 
+#
+#Smooth 'cause that's the way I will be 
+#And if you don't give a damn, then 
+#Why you starin' at me 
+#So get off 'cause I control the stage 
+#There's no dissin' allowed 
+#I'm in my own phase 
+#The girlies sa y they love me and that is ok 
+#And I can dance better than any kid n' play 
+#
+#Stage 2 -- Yea the one ya' wanna listen to 
+#It's off my head so let the beat play through 
+#So I can funk it up and make it sound good 
+#1-2-3 Yo -- Knock on some wood 
+#For good luck, I like my rhymes atrocious 
+#Supercalafragilisticexpialidocious 
+#I'm an effect and that you can bet 
+#I can take a fly girl and make her wet. 
+#
+#I'm like Samson -- Samson to Delilah 
+#There's no denyin', You can try to hang 
+#But you'll keep tryin' to get my style 
+#Over and over, practice makes perfect 
+#But not if you're a loafer. 
+#
+#You'll get nowhere, no place, no time, no girls 
+#Soon -- Oh my God, homebody, you probably eat 
+#Spaghetti with a spoon! Come on and say it! 
+#
+#VIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino 
+#Intoxicating so you stagger like a wino 
+#So punks stop trying and girl stop cryin' 
+#Vanilla Ice is sellin' and you people are buyin' 
+#'Cause why the freaks are jockin' like Crazy Glue 
+#Movin' and groovin' trying to sing along 
+#All through the ghetto groovin' this here song 
+#Now you're amazed by the VIP posse. 
+#
+#Steppin' so hard like a German Nazi 
+#Startled by the bases hittin' ground 
+#There's no trippin' on mine, I'm just gettin' down 
+#Sparkamatic, I'm hangin' tight like a fanatic 
+#You trapped me once and I thought that 
+#You might have it 
+#So step down and lend me your ear 
+#'89 in my time! You, '90 is my year. 
+#
+#You're weakenin' fast, YO! and I can tell it 
+#Your body's gettin' hot, so, so I can smell it 
+#So don't be mad and don't be sad 
+#'Cause the lyrics belong to ICE, You can call me Dad 
+#You're pitchin' a fit, so step back and endure 
+#Let the witch doctor, Ice, do the dance to cure 
+#So come up close and don't be square 
+#You wanna battle me -- Anytime, anywhere 
+#
+#You thought that I was weak, Boy, you're dead wrong 
+#So come on, everybody and sing this song 
+#
+#Say -- Play that funky music Say, go white boy, go white boy go 
+#play that funky music Go white boy, go white boy, go 
+#Lay down and boogie and play that funky music till you die. 
+#
+#Play that funky music Come on, Come on, let me hear 
+#Play that funky music white boy you say it, say it 
+#Play that funky music A little louder now 
+#Play that funky music, white boy Come on, Come on, Come on 
+#Play that funky music 
 
